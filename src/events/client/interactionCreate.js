3c4ebf76c13669e0f289fe64ed2f29f25cd590dd
Marc van Elswijk <9018248@student.zadkine.nl>
2025-06-27 22:39:40 +0200

    fixing

---
+++ src/events/client/interactionCreate.js
@@ -0,0 +1,24 @@
+const { InteractionType } = require(`discord.js`);
+
+module.exports = {
+    name: "interactionCreate",
+    async execute(interaction, client) {
+        if (interaction.isChatInputCommand()) {
+            const { commands } = client;
+            const { commandName } = interaction;
+            const command = commands.get(commandName);
+
+            if (!command) return;
+
+            try {
+                command.execute(interaction, client, profileData);
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({
+                    content: `Something went wrong with the KevayBot...`,
+                    ephemeral: true
+                });
+            }
+        }
+    },
+};
